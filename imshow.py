e68c6f5349b9ac739fa51449e3b6eb9958d8fc13
retsetera <77707053+retsetera@users.noreply.github.com>
2024-08-30 21:43:27 -0500

    made imshow work

--- imshow.py
+++ imshow.py
@@ -6,11 +6,15 @@ import cv2
 class imshow:
     def __init__(self,resolution):
         pygame.init()
-        self.screen = pygame.display.set_mode(resolution)
+        self.screen = pygame.display.set_mode((500,500), pygame.RESIZABLE)
         pygame.display.set_caption('dan')
+    
     def __call__(self,img):
+        for i in pygame.event.get():
+            if i.type == pygame.QUIT:
+                status = False
         self.screen.blit(pygame.surfarray.make_surface(np.flipud(np.rot90(cv2.cvtColor(img,cv2.COLOR_BGRA2RGB),1))), (0,0))
-        pygame.display.update()
+        pygame.display.flip()
 
 
 """imshower = imshow((1024,576))
